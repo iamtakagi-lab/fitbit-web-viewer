1cf8a070e7fa5dd67275faee6a78a6f874d6b4fb
takagi <iam.takagi.dev@gmail.com>
2021-09-04 07:42:45 +0900

    Support axios with SWR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
 import React, { Props, useEffect, useState } from 'react'
 import useSWR from 'swr';
-import { Fitbit } from '../components/fitbit';
+import Fitbit from '../components/fitbit';
 import { REFRESH_INTERVAL } from '../constants';
 import { Activity } from '../types';
-
+import axios from 'axios'
 
 const IndexPage: React.FC<{}> = () => {
-  const { data: activity } = useSWR<Activity>("/api/fitbit", {refreshInterval: REFRESH_INTERVAL}); 
+  const fetcher = url => axios.get(url).then(res => res.data)
+  const { data: activity } = useSWR<Activity>('/api/fitbit', fetcher, {refreshInterval: REFRESH_INTERVAL})
 
   if(!activity) return <p>loading...</p>
 
-  return ( 
-      <>
-        <Fitbit activity={activity}/>
-      </>
+  return (
+    <>
+      <Fitbit activity={activity}/>
+    </>
   )
 }
 
