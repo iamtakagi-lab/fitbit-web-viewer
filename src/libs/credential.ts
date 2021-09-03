869c2016656c7447f2bada691509de95d1b7b983
takagi <iam.takagi.dev@gmail.com>
2021-09-04 07:43:12 +0900

    Implement refresh token

--- src/libs/credential.ts
+++ src/libs/credential.ts
@@ -1,25 +1,24 @@
 import fs from 'fs'
 import path from 'path'
-import env from './env'
+import env from '../env'
 import db from './db'
+import doRefreshToken from '../libs/refreshToken'
+import Fitbit from '../components/fitbit'
+import useFitbit from './useFitbit'
+import { TOKEN_EXPIRE_TIME } from '../constants'
 
 export default class Credential {
 
     accessToken: string = ""
     refreshToken: string = ""
+    lastRefreshAt: number = 0
 
-    constructor() {
-
-        let ref
-
-        async() => {
-            ref = await db.collection(env.FIREBASE_COLLECTION_NAME).get()
-        }
-        
+    async load() {
+        const ref = await db.collection(env.FIREBASE_COLLECTION_NAME).get()
         const data = ref.docs.find(doc => doc.id === env.FIREBASE_DOCUMENT_ID).data()
-
         const accessToken = data.accessToken
-        const refreshToken= data.refreshToken
+        const refreshToken = data.refreshToken
+        const lastRefreshAt = data.lastRefreshAt
 
         if (
             accessToken == null ||
@@ -32,7 +31,16 @@ export default class Credential {
         if (accessToken != null && refreshToken != null) {
             this.setAccessToken(accessToken)
             this.setRefreshToken(refreshToken)
+            this.setLastRefreshAt(lastRefreshAt)
         }
+
+        //最後のトークンリフレッシュから1時間以上経過していたら更新する
+        const expiry = Date.now() + TOKEN_EXPIRE_TIME * 3600
+        if(expiry >= Date.now()) {
+            doRefreshToken(useFitbit(), this)
+        }
+ 
+        return this
     }
 
     setAccessToken(accessToken: string) {
@@ -41,6 +49,10 @@ export default class Credential {
 
     setRefreshToken(refreshToken: string) {
         this.refreshToken = refreshToken
+    }
+
+    setLastRefreshAt(lastRefreshAt: number) {
+        this.lastRefreshAt = lastRefreshAt
     }
 
     resetCredential() {
@@ -53,7 +65,8 @@ export default class Credential {
         const doc = ref.docs.find(doc => doc.id === env.FIREBASE_DOCUMENT_ID)
         doc.ref.set({
             "accessToken": this.accessToken,
-            "refreshToken": this.refreshToken
+            "refreshToken": this.refreshToken,
+            "lastRefreshAt": Date.now()
         })
     }
 }
