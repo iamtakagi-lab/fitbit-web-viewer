432f21e9913de9ead70afb42b8a5c0f645adf4a8
takagi <iam.takagi.dev@gmail.com>
2021-09-04 02:39:18 +0900

    Cleanup imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 
-import { ToastProvider } from 'react-toast-notifications'
 import { Layout } from '../components/layout'
 
 import '../components/tailwind.scss'
@@ -10,11 +9,9 @@ const App: React.FC<{
   Component: React.FC, pageProps: any}> = ({ Component, pageProps }) => {
   return (
     <>
-      <ToastProvider placement="top-center" autoDismissTimeout={5000}>
         <div className="container mx-auto text-left">
           <Layout Component={Component} pageProps={pageProps} />
         </div>
-      </ToastProvider>
     </>
   )
 }
