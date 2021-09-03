a85d3fbdd19f4c1cc151303f5a9cbd80439e607a
takagi <iam.takagi.dev@gmail.com>
2021-09-04 05:37:12 +0900

    Add FIREBASE_CLIENT_EMAIL

--- src/libs/env.ts
+++ src/libs/env.ts
@@ -17,11 +17,11 @@ export default {
     FITBIT_CLIENT_SECRET: process.env.FITBIT_CLIENT_SECRET
         ? process.env.FITBIT_CLIENT_SECRET
         : '',
-    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL
-        ? process.env.FIREBASE_CLIENT_EMAIL
-        : '',
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID
         ? process.env.FIREBASE_PROJECT_ID
+        : '',
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL
+        ? process.env.FIREBASE_CLIENT_EMAIL
         : '',
     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PROJECT_ID
         ? process.env.FIREBASE_PROJECT_ID.replace(/\\n/g, '\n')
