e5d622446d8c6c2b1579511dbb50c35ff91315ed
takagi <iam.takagi.dev@gmail.com>
2021-09-04 07:38:53 +0900

    Fix Fitbit.tsx

--- src/components/fitbit.tsx
+++ src/components/fitbit.tsx
@@ -1,14 +1,18 @@
+import env from '../env'
 import * as React from 'react'
 import { Activity } from '../types'
 
-export const Fitbit: React.FC<{activity: Activity}> = ({activity}) => {
-
+const Fitbit: React.FC<{activity: Activity}> = ({activity}) => {
   return (
    <>
+    <p>{env.NAME} - 人体</p>
     <p>❤ 心拍数: {activity.heartrate}</p>
     <p>🔥 消費済みカロリー: {activity.calories}</p>
     <p>👟 歩いた距離: {activity.miles}</p>
     <p>📐 歩いた階数: {activity.floors}</p>
+    <p>💤 睡眠時間: {activity.sleep}</p>
    </>
   )
 }
+
+export default Fitbit
