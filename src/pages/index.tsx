e3edf29ed9133ecc1d76dff6f6d6325c4a0aa63f
takagi <iam.takagi.dev@gmail.com>
2021-09-04 02:47:26 +0900

    Add loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import React, { Props, useEffect, useState } from 'react'
 import useSWR from 'swr';
 import { Fitbit } from '../components/fitbit';
 import { REFRESH_INTERVAL } from '../constants';
+import { Activity } from '../types';
 
 
 const IndexPage: React.FC<{}> = () => {
-  const { data: activity, mutate: mutate } = useSWR("/api/fitbit", {refreshInterval: REFRESH_INTERVAL}); 
+  const { data: activity } = useSWR<Activity>("/api/fitbit", {refreshInterval: REFRESH_INTERVAL}); 
+
+  if(!activity) return <p>loading...</p>
 
   return ( 
       <>
