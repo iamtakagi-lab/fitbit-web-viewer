cb5c492f91033e09672ac7e3314b8feca9c39f97
takagi <iam.takagi.dev@gmail.com>
2021-09-04 09:21:36 +0900

    Resolve firestore ratelimit

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
 export const TOKEN_EXPIRE_TIME = 3600
-export const REFRESH_INTERVAL = 1000 * 30
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 const IndexPage: React.FC<{}> = () => {
   const fetcher = url => axios.get(url).then(res => res.data)
-  const { data: activity } = useSWR<Activity>('/api/fitbit', fetcher, {refreshInterval: REFRESH_INTERVAL})
+  const { data: activity } = useSWR<Activity>('/api/fitbit', fetcher)
 
   if(!activity) return <p>loading...</p>
 
