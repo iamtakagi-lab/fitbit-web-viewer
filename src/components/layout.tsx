3c9deac778ed349ae9d7a1c7f83cef907a75d507
takagi <iam.takagi.dev@gmail.com>
2021-09-04 10:18:01 +0900

    Fix style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,13 +4,10 @@ export const Layout: React.FC<{
   Component: React.FC, pageProps: any
 }> = ({ Component, pageProps }) => (
   <div className="min-h-screen w-full flex flex-col text-gray-800 leading-loose">
-    <div className="relative z-20" id="modal-container" />
-    <div className="flex-1 mt-5">
-      <Component {...pageProps} />
-    </div>
     <div className="mt-5">
+      <Component {...pageProps} />
       <a
-        className="text-sm underline text-blue-500"
+        className="text-sm underline text-blue-500 mt-5"
         href="https://github.com/iamtakagi/fitbit-insights"
       >https://github.com/iamtakagi/fitbit-insights</a>
     </div>
